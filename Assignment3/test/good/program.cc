8732871fa215029f6a6d4786b23b55759cddb96a
Michael Kulinich <kulinich@chapman.edu>
2021-05-03 09:02:25 -0700

    build

---
+++ Assignment3/test/good/program.cc
@@ -0,0 +1,14 @@
+int main() {
+    // int x = 0;
+    // printInt(2 /24);
+    // string x = "d";
+    int x = 3;
+    x = 3;
+    bool z = true;
+    // printInt(x++);
+    // printInt(x);
+    // printInt(++x);
+    // printInt(true);
+    true && true;
+    return (0) && 1;
+}
